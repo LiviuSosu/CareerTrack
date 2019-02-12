45bb8a7d98ac399c3765a3d4cecf779dbecff77a
Liviu Sosu <sosuliviu@gmail.com>
2019-02-12 12:32:21 +0200

    Done.

--- CareerTrack.Domain/Entities/User.cs
+++ CareerTrack.Domain/Entities/User.cs
@@ -6,5 +6,6 @@ namespace CareerTrack.Domain.Entities
     {
         public Guid Id { get; set; }
         public string UserName { get; set; }
+        public string Password { get; set; }
     }
 }
