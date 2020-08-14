7cc7a0472190ae4ba596d7884fe36da1eefa808a
LiviuSosu <sosuliviu@gmail.com>
2020-08-14 23:06:58 +0300

    fixed teh unit tests

--- CareerTrack.Application.Tests/Articles/Command/CreateArticleTest.cs
+++ CareerTrack.Application.Tests/Articles/Command/CreateArticleTest.cs
@@ -1,4 +1,4 @@
-﻿using CareerTrack.Application.Articles.Commands.Create;
+﻿using CareerTrack.Application.Handlers.Articles.Commands.Create;
 using CareerTrack.Application.Tests.Articles.Query;
 using MediatR;
 using System.Linq;
