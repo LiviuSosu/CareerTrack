f78c063f1e5ccc8a8ae054786b6a704acb8a1bf1
LiviuSosu <sosuliviu@gmail.com>
2020-08-15 19:33:16 +0300

    corrected the code

--- CareerTrack.Application/Handlers/ValidatorCommandBase.cs
+++
@@ -1,9 +0,0 @@
-﻿using FluentValidation;
-
-namespace CareerTrack.Application.Handlers
-{
-    public class ValidatorCommandBase<T> : AbstractValidator<T>
-    {
-        public ValidatorCommandBase() { }
-    }
-}
