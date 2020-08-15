6e49f3fe01333d4baa526ba5b3e835c929cee5a9
LiviuSosu <sosuliviu@gmail.com>
2020-08-15 19:27:52 +0300

    done

--- CareerTrack.Application/Handlers/Articles/Commands/Update/UpdateArticleCommandValidator.cs
+++ CareerTrack.Application/Handlers/Articles/Commands/Update/UpdateArticleCommandValidator.cs
@@ -1,7 +1,13 @@
-﻿
+﻿using FluentValidation;
+
 namespace CareerTrack.Application.Handlers.Articles.Commands.Update
 {
-    public class UpdateArticleCommandValidator : ArticleCommandBaseValidator
+    public class UpdateArticleCommandValidator : AbstractValidator<UpdateArticleCommand>
     {
+        public UpdateArticleCommandValidator()
+        {
+            RuleFor(article => article.Title).NotEmpty();
+            RuleFor(article => article.Id).NotNull();
+        }
     }
 }
