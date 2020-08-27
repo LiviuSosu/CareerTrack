9fade89d4a562299bbbe1eba085cda4f343d4483
LiviuSosu <sosuliviu@gmail.com>
2020-08-27 22:54:34 +0300

    started the Login refactoring. Most important: generic validator added

---
+++ CareerTrack.Application/Handlers/Users/Commands/Login/UserLoginCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace CareerTrack.Application.Handlers.Users.Commands.Login
+{
+    public class UserLoginCommand : IRequest
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ CareerTrack.Application/Handlers/Users/Commands/Login/UserLoginCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using CareerTrack.Application.Handlers.Articles;
+using CareerTrack.Persistance;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CareerTrack.Application.Handlers.Users.Commands.Login
+{
+    public class UserLoginCommandHandler : BaseHandler<UserLoginCommand, Unit>, IRequestHandler<UserLoginCommand, Unit>
+    {
+        public UserLoginCommandHandler(CareerTrackDbContext context) : base(context)
+        {
+
+        }
+
+        public new async Task<Unit> Handle(UserLoginCommand request, CancellationToken cancellationToken)
+        {
+            await Task.Delay(200);
+            return Unit.Value;
+            //_repoWrapper.Article.Create(_mapper.Map<Article>(request));
+            //await _repoWrapper.SaveAsync();
+            //return Unit.Value;
+        }
+    }
+}
